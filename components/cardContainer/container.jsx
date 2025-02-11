095283caba2511db721a88c010155f7f1abeba64
Suikyou <sethbardilas@gmail.com>
2025-02-11 17:38:44 +0800

    tailwind.config.mjs - renamed parts to be camelCase layout.js - renamed only the import navbar to be navBar (camelCase)
    Created card.jsx and container.jsx inside the folder of components > cardContainer > jsx files
    - card.jsx not flexbox anymore
    
    navBar.jsx - replaced divs with container from components.
    - also changed the imports to be @
    
    Typography.module.css - Renamed parts to be camelCase
    - also added a white font collor, separated via comment to not mix with standards

---
+++ components/cardContainer/container.jsx
@@ -0,0 +1,8 @@
+// components/Container.jsx
+import React from "react";
+
+const Container = ({ children, className = "" }) => {
+    return <div className={className}>{children}</div>;
+};
+
+export default Container;
--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -1,28 +1,33 @@
+// components/Navbar.jsx
 import Image from "next/image";
+import Container from "@/components/cardContainer/container";
+import { PrimaryLabel, SecondaryTitle, ActionLink } from "@/components/reusables/typography/Typography";
+import styles from "@/components/reusables/typography/Typography.module.css";
 
 const Navbar = () => {
     return (
-        <div className="bg-[#100389] text-white w-full py-5 fixed top-0 left-0 flex items-center justify-between px-6 z-50">
+        <Container className="bg-primaryDark text-white w-full py-4 fixed top-0 left-0 flex items-center justify-between px-10 z-50">
             {/* Left Section: Logos and Text */}
-            <div className="flex items-center space-x-4">
-                <Image src="/icons/LARLogo.SVG" alt="LRA Logo" width={80} height={32} />
-                <div className="text-center">
-                    <span className="block text-sm">Republic of the Philippines</span>
-                    <hr className="border-t border-white my-1" />
-                    <span className="block text-sm">Department of Justice</span>
-                    <span className="block text-lg font-bold">LAND REGISTRATION AUTHORITY</span>
-                </div>
-                <Image src="/icons/digital-philippines.png" alt="Digital Philippines Logo" width={80} height={32} />
-            </div>
+            <Container className="flex items-center gap-4">
+                <Image src="/icons/LARLogo.SVG" alt="LRA Logo" width={60} height={60} />
+                
+                <Container className={styles.whiteText}>
+                    <PrimaryLabel>Republic of the Philippines</PrimaryLabel>
+                    <hr className="border-t border-white my-1 w-full" />
+                    <PrimaryLabel>Department of Justice</PrimaryLabel>
+                    <SecondaryTitle className="font-bold">LAND REGISTRATION AUTHORITY</SecondaryTitle>
+                </Container>
 
-            {/* PLACEHOLDER NO NAVIGATION OR LOGIC YET: Check Payment Status */}
-            <div className="ml-auto mr-20 mt-4"> 
-                <a href="#" className="text-white text-sm hover:underline">
-                    Check Payment Status
-                </a>
-            </div>
-        </div>
+                <Image src="/icons/digital-philippines.png" alt="Digital Philippines Logo" width={60} height={60} />
+            </Container>
+
+            {/* Check Payment Status Button */}
+            <Container className="ml-auto">
+                <ActionLink className="text-actionLink">Check Payment Status</ActionLink>
+            </Container>
+        </Container>
     );
-}
+};
 
 export default Navbar;
+

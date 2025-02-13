2ea876b93f5f464e3785eea6ced6316fa0a623e6
Suikyou <sethbardilas@gmail.com>
2025-02-13 09:57:00 +0800

    minor adjustments and minor cleaning

--- components/reusables/cardContainer/card.jsx
+++ components/reusables/cardContainer/card.jsx
@@ -1,8 +1,14 @@
-// components/Card.jsx
 import React from "react";
 
-const Card = ({ children, className = "" }) => {
-    return <div className={className}>{children}</div>;
+const Card = ({ children, disableDefaultStyles, className = "", ...props }) => {
+  const baseClasses = disableDefaultStyles 
+    ? ""
+    : "p-6 rounded-lg shadow-md bg-white overflow-hidden transition-all duration-200 hover:shadow-lg text-center";
+  return (
+    <div className={`${baseClasses} ${className}`} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
